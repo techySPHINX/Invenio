16f66cd09f1ff49554f7764a7f1aa114aa2d293f
techySphinx <138506773+techySPHINX@users.noreply.github.com>
2025-06-14 12:26:44 +0530

    refactor : seed and dependencies

--- backend/dist/src/index.js
+++ backend/dist/src/index.js
@@ -8,6 +8,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
 const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
 const expenseRoutes_1 = __importDefault(require("./routes/expenseRoutes"));
 const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
@@ -15,6 +16,7 @@ const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
 /* Configurations */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)({ origin: "http://localhost:3000", credentials: true }));
 app.use(express_1.default.json());
 app.use((0, helmet_1.default)());
 app.use(helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -29,8 +31,5 @@ app.use("/expenses", expenseRoutes_1.default); // http://localhost:8000/expenses
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, "0.0.0.0", () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`🚀 Server running on http://localhost:${port}`);
 });
-function cors() {
-    throw new Error("Function not implemented.");
-}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import cors from "cors"; 
 
 import dashboardRoutes from "./routes/dashboardRoutes";
 import expenseRoutes from "./routes/expenseRoutes";
@@ -12,13 +13,15 @@ import userRoutes from "./routes/userRoutes";
 /* Configurations */
 dotenv.config();
 const app = express();
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 /* ROUTES */
 app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
@@ -29,9 +32,5 @@ app.use("/expenses", expenseRoutes); // http://localhost:8000/expenses
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, "0.0.0.0", () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`🚀 Server running on http://localhost:${port}`);
 });
-
-function cors(): any {
-  throw new Error("Function not implemented.");
-}
